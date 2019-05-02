3af4f88476b2d8db8929150f8b2b5ce4becdb98b
Ivo Saavedra <ivo_saavedra@outlook.com>
2019-05-02 10:12:29 +0100

    Changes to constructors

--- Address.h
+++ Address.h
@@ -8,9 +8,15 @@ using namespace std;
 class Address {
 	public:
 		Address();
-		Address(string street, int door, string floor, string zipCode, string location);
+		Address(string street, unsigned int door, string floor, string zipCode, string location);
 		Address(string addressStr); // "street / door / floor / zipCode / location"
 		string toString();
+		string getStreet() const;
+		unsigned int getDoor() const;
+		string getFloor() const;
+		string getZipCode() const;
+		string getLocation() const;
+
 	private:
 		string street;
 		unsigned int door;
